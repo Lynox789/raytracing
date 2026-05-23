e65f77d120c1514156134b3e49ea7fab0a363ca7
sebastien <dumur.seb@gmail.com>
2026-05-24 00:23:20 +0200

    Implementation of a working window with SDL2

--- raytracing.c
+++ raytracing.c
@@ -1,9 +1,21 @@
 //Fichier créé le 24/05/2026 à 00:04:36
 #include <stdio.h>
-#include <stdlib.h>
 #include <SDL2/SDL.h>
 
+#define WIDTH 900
+#define HEIGHT 600
 
-int main(){
+#define WHITE 0xffffffff
+#define BLACK 0x00000000
+
+int main(int argc, char *argv[]){
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Raytracing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+    
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+    SDL_Rect rect = (SDL_Rect) {200, 200, 200, 200};
+    SDL_FillRect(surface, &rect, WHITE);
+    
+    SDL_Delay(5000);
 	return 0;
 }
